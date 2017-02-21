56bfe1c87db406cdc9c6148ba85164dfbdd0a0e6
Andrew Nguyen <nguy575@usc.edu>
2017-02-20 19:48:01 -0800

    Static AutoComplete Search for Landing Page

---
+++ js/landing-page.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Autosuggest from 'react-autosuggest';
+
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'C#',
+    year: 2000
+  },
+  {
+    name: 'C++',
+    year: 1983
+  },
+  {
+    name: 'Clojure',
+    year: 2007
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  {
+    name: 'Go',
+    year: 2009
+  },
+  {
+    name: 'Haskell',
+    year: 1990
+  },
+  {
+    name: 'Java',
+    year: 1995
+  },
+  {
+    name: 'Javascript',
+    year: 1995
+  },
+  {
+    name: 'Perl',
+    year: 1987
+  },
+  {
+    name: 'PHP',
+    year: 1995
+  },
+  {
+    name: 'Python',
+    year: 1991
+  },
+  {
+    name: 'Ruby',
+    year: 1995
+  },
+  {
+    name: 'Scala',
+    year: 2003
+  }
+];
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };    
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
+  }
+
+  onChange(event, { newValue, method }) {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested({ value }) {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested() {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Search for Workflows",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest 
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} />
+    );
+  }
+}
+
+ReactDOM.render(<SearchBar />, document.getElementById('search-bar'));
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Title from './Title.jsx';
+import SearchBar from './landing-page.jsx'
 
 ReactDOM.render(<Title />, document.getElementById("title"));
