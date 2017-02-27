582ab92d473cef5579a09dd54e48e2dd7d86b940
huyngoo <huyngo@usc.edu>
2017-02-27 14:24:39 -0800

    Additional features for visualization - make visualization take up 50% of screen width, center the graph - allow users to zoom in and out of graph, click and hold to drag graph around - hover over a node changes opacity of it - styling: changing spacing between ranks on graph/ font color of processes

--- js/visualization.js
+++ js/visualization.js
@@ -5,11 +5,21 @@ d3.json('./../js/data.json', function(data) {
     var processNodeIndices = {};
     var putNodeIndices = {};
     
+    /*
+        @params: string processName, string inputName
+        - inserts the inputName to the list of inputs that the processName maps
+        to within the object processInputMapping
+    */
     var addInputProcess = function(processName, inputName) {
         processInputMapping[processName] = processInputMapping[processName] || [];
         processInputMapping[processName].push(inputName);
     }
     
+    /*
+        @params: string processName, string outputName
+        - inserts the outputName to the list of outputs that the processName maps
+        to within the object processOutputMapping
+    */
     var addOutputProcess = function(processName, outputName) {
         processOutputMapping[processName] = processOutputMapping[processName] || [];
         processOutputMapping[processName].push(outputName);
@@ -23,6 +33,11 @@ d3.json('./../js/data.json', function(data) {
         return uri.substring(uri.lastIndexOf('CE_')+3, uri.length).toLowerCase();
     }
     
+    
+    /*
+        @params: dagreD3 graph
+        - pulls from JSON result and sets nodes according to whether or not they are labeled as inputs, outputs, or processes
+    */
     var mapNodesEdges = function(graph) {
         var j = 0;
         for (var i = 0; i < results.length; i++) {
@@ -37,6 +52,7 @@ d3.json('./../js/data.json', function(data) {
                 stepToDisplay = stepToDisplay.substring(0, stepToDisplay.lastIndexOf('node')) == '' ? stepToDisplay : stepToDisplay.substring(0, stepToDisplay.lastIndexOf('node'));
                 vis.setNode(j, { 
                     label: stepToDisplay,
+                    labelStyle: "fill: #000",
                     style: "fill: #FFCC99;",
                     uri: step
                 });
@@ -51,6 +67,7 @@ d3.json('./../js/data.json', function(data) {
                     var inputToDisplay = stripNameFromURI(input);
                     vis.setNode(j, { 
                         label: inputToDisplay,
+                        labelStyle: "fill: #FFF",
                         shape: 'ellipse',
                         style: "fill: #003366;",
                         uri: input
@@ -67,6 +84,7 @@ d3.json('./../js/data.json', function(data) {
                     var outputToDisplay = stripNameFromURI(output);
                     vis.setNode(j, { 
                         label: outputToDisplay,
+                        labelStyle: "fill: #FFF",
                         shape: 'ellipse',
                         style: "fill: #003366;",
                         uri: output
@@ -78,49 +96,101 @@ d3.json('./../js/data.json', function(data) {
         }
     }
     
+    /*
+        params: dagreD3 vis
+        - connects nodes to each other via node numbers from processInputMapping
+        and processOutputMapping
+    */
+    var setGraphEdges = function (vis) {
+        for (var process in processInputMapping) {
+            for (var i = 0; i < processInputMapping[process].length; i++) {
+                vis.setEdge(putNodeIndices[processInputMapping[process][i]], processNodeIndices[process], {
+                    style: "stroke: #000; fill:none"
+                });
+            }
+        }
+        for (var process in processOutputMapping) {
+            for (var i = 0; i < processOutputMapping[process].length; i++) {
+                vis.setEdge(processNodeIndices[process], putNodeIndices[processOutputMapping[process][i]], {
+                    style: "stroke: #000; fill:none"
+                });
+            }
+        }
+    }
+    
+    
+    /*
+        renders graph
+    */
     var vis = new dagreD3.graphlib.Graph()
-        .setGraph({})
+        .setGraph({
+            nodesep: 10,
+            ranksep: 20,
+        })
         .setDefaultEdgeLabel(function() { return {} });
     
     mapNodesEdges(vis);
-    
-    for (var process in processInputMapping) {
-        for (var i = 0; i < processInputMapping[process].length; i++) {
-            vis.setEdge(putNodeIndices[processInputMapping[process][i]], processNodeIndices[process], {
-                style: "stroke: #000; fill:none"
-            });
-        }
-    }
-    
-    for (var process in processOutputMapping) {
-        for (var i = 0; i < processOutputMapping[process].length; i++) {
-            vis.setEdge(processNodeIndices[process], putNodeIndices[processOutputMapping[process][i]], {
-                style: "stroke: #000; fill:none"
-            });
-        }
-    }
+    setGraphEdges(vis);
     
     // Create the renderer
     var render = new dagreD3.render();
 
     // Set up an SVG group so that we can translate the final graph.
-    var svg = d3.select("svg").attr('width','100%').attr('height','100%'), svgGroup = svg.append("g");
+    var svg = d3.select("svg").attr('width','50%').attr('height','100%'), svgGroup = svg.append("g");
+    
+    // Set up zoom support
+    var zoom = d3.behavior.zoom().on("zoom", function() {
+        svgGroup.attr("transform", "translate(" + d3.event.translate + ")" +
+                                    "scale(" + d3.event.scale + ")");
+    });
+    svg.call(zoom);
 
     // Run the renderer. This is what draws the final graph.
     render(svgGroup, vis);
     
     //centers graph and calculates top margin of graph based on screen size
-    var xCenterOffset = (screen.width - vis.graph().width) / 2;
+    var xCenterOffset = (document.getElementsByClassName('visualization-container')[0].clientWidth / 2 - vis.graph().width) / 2;
     var yTopMargin = screen.height * .05;
-    svgGroup.attr("transform", "translate(" + xCenterOffset + ", " + yTopMargin + ")");
-    svg.attr("height", vis.graph().height + yTopMargin + 40);
+    var scale = 1;
+    zoom
+      .translate([xCenterOffset, 20])
+      .scale(scale)
+      .event(svg);
+    svg.attr('height', vis.graph().height * scale + yTopMargin);
     
-    d3.select('svg g g g.nodes').attr('fill', '#FFF');
-    
+    setupNodeOnClick(svg);
+    addHover(svg);
+});
+
+
+/*
+    @params: d3 svg
+    - setup on click process for each node
+*/
+var setupNodeOnClick = function (svg) {
     //setup on click listeners for every node
     svg.selectAll("g.node").on("click", function(id) {
         //call function from processInfoScript.js to add 1 table to roulette
         addProcessInfo();
         //TODO: connect to Tiff's roulette logic and populate her roulette info
     });
-});
+}
+
+/*
+    @params: d3 svg
+    - add hover opacity effect for each node when it is moused over, and returns back to normal opacity when it is moused out
+*/
+var addHover = function(svg) {
+    //change opacity of nodes based on whether or not the mouse is hovering over them
+    svg.selectAll('g.node').on('mouseover', function(id) {
+        svg.selectAll('g.node').style('opacity', function(id1){
+            if (id == id1) return 0.7;
+        });
+    });
+    
+    svg.selectAll('g.node').on('mouseout', function(id) {
+        svg.selectAll('g.node').style('opacity', function(id1){
+            return 1;
+        });
+    });
+}
