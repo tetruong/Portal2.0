07b39452c171760fcbda54adcf2e5e6eda02cd74
huyngoo <huyngo@usc.edu>
2017-03-05 21:21:27 -0800

    Making ajax request for workflow inputs

--- js/query.js
+++ js/query.js
@@ -1,5 +1,7 @@
+/*
+    @params: function "handler" that can be called when ajax call finishes
+*/
 var populateSearchBar = function(handler) {
-    var json = {};
     $.ajax({
         url: 'http://seagull.isi.edu:3030/ds/query?query=select+%3Fwf+%3Flabel+from+%3Curn%3Ax-arq%3AUnionGraph%3E+where%7B%0D%0A%0D%0A%3Fwf+a+%3Chttp%3A%2F%2Fwww.opmw.org%2Fontology%2FWorkflowTemplate%3E.%0D%0A%3Fwf++%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23label%3E+%3Flabel.%0D%0A%7D&output=json&stylesheet=',
         type: 'GET',
@@ -9,7 +11,23 @@ var populateSearchBar = function(handler) {
             handler({});
         },
         success: function(res){ 
+            //success condition executes if ajax call goes through- we call the function handler, passing in the ajax response "res"
             handler(res);
+        }
+    });
+}
+
+var getInputs = function(workflow, handler) {
+    var sparql = 'select ?input from <urn:x-arq:UnionGraph> where{?input <http://www.opmw.org/ontology/isVariableOfTemplate> <' + workflow + '>.?p <http://www.opmw.org/ontology/uses> ?input.FILTER NOT EXISTS {?input<http://www.opmw.org/ontology/isGeneratedBy> ?p2.}}';
+    
+    var endpointURI = "http://seagull.isi.edu:3030/ds/query?query=" + escape(sparql) + "&format=json";
+    
+    $.ajax({
+        dataType :'jsonp',
+        jsonp :'callback',
+        url :endpointURI,
+        success: function(res) {
+            console.log(res);
         }
     });
 }
