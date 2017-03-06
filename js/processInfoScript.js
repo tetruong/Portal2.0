70a52e6f7b92f5b3bc716e00999d72f6d8b7405e
Tiffany Truong <tetruong@usc.edu>
2017-03-05 23:04:20 -0800

    process info correctly populates input and output variable names

--- js/processInfoScript.js
+++ js/processInfoScript.js
@@ -22,6 +22,39 @@ function addProcessInfo(processURI, inputsArray, outputsArray) {
         // remove a 
     }
     // check that process div thing is not over count;
+    
+    // set header name
+    var processName = stripNameFromURI(processURI);
+    console.log(processName);
+    var nameHeader = document.getElementById("name_of_process_header");
+    nameHeader.innerHTML = processName;
+    
+    console.log(inputsArray);
+    console.log(outputsArray);
+    // Populate the table with variable data
+    var l = inputsArray.length;
+    if (outputsArray.length > l) {
+        l = outputsArray.length;
+    }
+    // adding inputs and outputs to the table
+    var tableBody = document.getElementById("process_info_table_body");
+    var newTableBody = document.createElement("tbody");
+    newTableBody.setAttribute("id", "process_info_table_body");
+    for (i = 0; i < l; i++) {
+        var row = newTableBody.insertRow(-1);
+        // add input variable name and add output variable name (if applicable)
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        if (i < inputsArray.length) {
+            cell1.innerHTML = stripNameFromURI(inputsArray[i]);
+        }
+        if (i < outputsArray.length) {
+            cell2.innerHTML = stripNameFromURI(outputsArray[i]);
+        }
+    }
+    console.log(tableBody.parentNode);
+    tableBody.parentNode.replaceChild(newTableBody, tableBody);
+    
     var newProcessInfoDiv = document.createElement("div"); // create a new div
     newProcessInfoDiv.setAttribute("id", processInfosIndex);
     newProcessInfoDiv.setAttribute("class", "processInfo");
@@ -29,11 +62,10 @@ function addProcessInfo(processURI, inputsArray, outputsArray) {
     var layoutDivCopy = layoutDiv.cloneNode(true);
     layoutDivCopy.style.display = "block";
     newProcessInfoDiv.innerHTML = layoutDivCopy.innerHTML;
-    document.body.appendChild(newProcessInfoDiv);
     processInfosCount = processInfosCount + 1;
     processInfosIndex = processInfosIndex + 1;
+    document.body.appendChild(newProcessInfoDiv);
     console.log(processInfosCount);
-    
     
 }
     
